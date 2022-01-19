f4152671c621d9daad8752974af7e8b8a823f7f2
Tsogtoo <tsogtbaatar.e13@gmail.com>
2022-01-19 14:51:30 +0800

    projectiin anhnii huvisagchdiig uusgesen.random function ashiglav

---
+++ app.js
@@ -0,0 +1,15 @@
+// //Тоглогчийн ээлжийг хадгалах хувьсагч
+// var activePlayer=1;
+// //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+// var scores=[0,0];
+// //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+// var roundScore=0;
+// //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй,1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+// var dice=Math.floor(Math.random()*6)+1;
+// window.document.querySelector(".dice").style.display="none";
+function hello(){
+    console.log('Sain uu');
+}
+hello(function(){
+    console.log('jja');
+});
