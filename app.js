070d43cd1f5bbfc9322a22d1f31481d99c01f845
Tsogtoo <tsogtbaatar.e13@gmail.com>
2022-01-20 21:09:57 +0800

    toglogchiin eeljiig solidog bolgono

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 //Тоглогчийн ээлжийг хадгалах хувьсагч
-var activePlayer = 1;
+var activePlayer = 0;
 //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
 var scores = [0, 0];
 //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
@@ -13,9 +13,31 @@ window.document.getElementById("current-1").textContent = 0;
 window.document.querySelector(".dice").style.display = "none";
 window.document
   .querySelector(".btn-roll")
+  //Шоог шидэх эвэнт листенер
   .addEventListener("click", function () {
+    //Math сангийн floor,random функцыг ашиглаж шоог санамсаргүйгээр буулгана
     var dice = Math.floor(Math.random() * 6) + 1;
     var diceDom = document.querySelector(".dice");
+    //Шооны зургийг вэб дээр гаргаж ирнэ
     diceDom.style.display = "block";
+    //Буусан санасаргүй тоонд харгалзах зургийг вэб дээр гаргаж ирнэ
     diceDom.src = "dice-" + dice + ".png";
+    //Буусан тоо нь 1-ээс ялгаатай бол идэвхтэй тоглогчийнээлжийн оноог нэмэгдүүлнэ.
+    if (dice !== 1) {
+      //Буусан тоог идэвхтэй тоглогчид нэмнэ.
+      roundScore += dice;
+      document.getElementById("current-" + activePlayer).textContent =
+        roundScore;
+    } else {
+      //1 буусан тул тоглогчийн ээлжийг энэ хэсэгт солино.
+      //Хэрэв идэвхитэй тоглогч нь 0 байвал идэвхитэй тоглогчийг 1 болго
+      //Үгүй бол идэвхитэй тоглогчийг 0 болго
+      roundScore = null;
+      document.getElementById("current-" + activePlayer).textContent = 0;
+      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+      document.querySelector(".player-0-panel").classList.toggle("active");
+      document.querySelector(".player-1-panel").classList.toggle("active");
+      document.querySelector(".dice").style.display = "none";
+    }
   });
+document.querySelector(".btn-hold").addEventListener("click", function () {});
