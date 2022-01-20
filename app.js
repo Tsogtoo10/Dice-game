4d984820af44bcbb1b412da434632977cdca07cd
Tsogtoo <tsogtbaatar.e13@gmail.com>
2022-01-20 15:36:36 +0800

    shoog shiddeg bolgoh

--- app.js
+++ app.js
@@ -1,15 +1,21 @@
-// //Тоглогчийн ээлжийг хадгалах хувьсагч
-// var activePlayer=1;
-// //Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
-// var scores=[0,0];
-// //Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
-// var roundScore=0;
-// //Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй,1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
-// var dice=Math.floor(Math.random()*6)+1;
-// window.document.querySelector(".dice").style.display="none";
-function hello(){
-    console.log('Sain uu');
-}
-hello(function(){
-    console.log('jja');
-});
+//Тоглогчийн ээлжийг хадгалах хувьсагч
+var activePlayer = 1;
+//Тоглогчдын цуглуулсан оноог хадгалах хувьсагч
+var scores = [0, 0];
+//Тоглогчийн ээлжиндээ цуглуулж байгаа оноог хадгалах хувьсагч
+var roundScore = 0;
+//Шооны аль талаараа буусныг хадгалах хувьсагч хэрэгтэй,1-6 гэсэн утгыг энэ хувьсагчид санамсаргүйгээр үүсгэж өгнө.
+
+window.document.getElementById("score-0").textContent = 0;
+window.document.getElementById("score-1").textContent = 0;
+window.document.getElementById("current-0").textContent = 0;
+window.document.getElementById("current-1").textContent = 0;
+window.document.querySelector(".dice").style.display = "none";
+window.document
+  .querySelector(".btn-roll")
+  .addEventListener("click", function () {
+    var dice = Math.floor(Math.random() * 6) + 1;
+    var diceDom = document.querySelector(".dice");
+    diceDom.style.display = "block";
+    diceDom.src = "dice-" + dice + ".png";
+  });
