a0fd95352dfd100f7bacbbf707b123e26104546b
Tsogtoo <tsogtbaatar.e13@gmail.com>
2022-01-22 17:21:26 +0800

    new game tovchiig ajillagaand oruulna

--- app.js
+++ app.js
@@ -11,6 +11,14 @@ window.document.getElementById("score-1").textContent = 0;
 window.document.getElementById("current-0").textContent = 0;
 window.document.getElementById("current-1").textContent = 0;
 window.document.querySelector(".dice").style.display = "none";
+function eeljSolih() {
+  roundScore = 0;
+  document.getElementById("current-" + activePlayer).textContent = 0;
+  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+  document.querySelector(".player-0-panel").classList.toggle("active");
+  document.querySelector(".player-1-panel").classList.toggle("active");
+  document.querySelector(".dice").style.display = "none";
+}
 window.document
   .querySelector(".btn-roll")
   //Шоог шидэх эвэнт листенер
@@ -32,12 +40,23 @@ window.document
       //1 буусан тул тоглогчийн ээлжийг энэ хэсэгт солино.
       //Хэрэв идэвхитэй тоглогч нь 0 байвал идэвхитэй тоглогчийг 1 болго
       //Үгүй бол идэвхитэй тоглогчийг 0 болго
-      roundScore = null;
-      document.getElementById("current-" + activePlayer).textContent = 0;
-      activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
-      document.querySelector(".player-0-panel").classList.toggle("active");
-      document.querySelector(".player-1-panel").classList.toggle("active");
-      document.querySelector(".dice").style.display = "none";
+      eeljSolih();
     }
   });
-document.querySelector(".btn-hold").addEventListener("click", function () {});
+//Hold товчны эвент листенер
+document.querySelector(".btn-hold").addEventListener("click", function () {
+  //Уг тоглогчийн цуглуулсан ээлжний оноог глобаль оноон дээр нь нэмж өгнө
+  scores[activePlayer] += roundScore;
+  //Уг тоглогч хожсон эсэхийг шалгах
+  document.getElementById("score-" + activePlayer).textContent =
+    scores[activePlayer];
+  //Ээлжийн оноог нь 0 болгоно
+  if (scores[activePlayer] >= 100) {
+    document.getElementById("name-" + activePlayer).textContent = "Winner";
+    document
+      .querySelector(".player-" + activePlayer + "-panel")
+      .classList.add("winner");
+  } else {
+    eeljSolih();
+  }
+});
